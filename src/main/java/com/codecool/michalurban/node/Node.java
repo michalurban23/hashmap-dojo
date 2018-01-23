9f0328cca68f29a5b63c7b942931f8c9d7b9bc4a
Michał Urban <michalurban23@gmail.com>
2018-01-23 08:05:06 +0100

    Add node model

--- src/main/java/com/codecool/michalurban/node/Node.java
+++ src/main/java/com/codecool/michalurban/node/Node.java
@@ -2,4 +2,33 @@ package com.codecool.michalurban.node;
 
 public class Node {
 
+    private String key;
+    private Integer value;
+
+    public Node(String key, Integer value) {
+
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+
+        return key;
+    }
+
+    public void setKey(String key) {
+
+        this.key = key;
+    }
+
+    public Integer getValue() {
+
+        return value;
+    }
+
+    public void setValue(Integer value) {
+
+        this.value = value;
+    }
+
 }
